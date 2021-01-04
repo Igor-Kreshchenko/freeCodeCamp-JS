e88868390a9da454c89aa98d31bd23d758ac59cd
Igor Kreshchenko <Igor.kreshchenko@gmail.com>
2021-01-04 20:13:53 +0200

    tasks 71-80

--- js/71-80.js
+++ js/71-80.js
@@ -1,1 +1,231 @@
 // 71
+function orderMyLogic(val) {
+  if (val < 5) {
+    return "Less than 5";
+  } else if (val < 10) {
+    return "Less than 10";
+  } else {
+    return "Greater than or equal to 10";
+  }
+}
+
+orderMyLogic(7);
+
+// 72
+function testSize(num) {
+  if (num < 5) {
+    return "Tiny";
+  } else if (num < 10) {
+    return "Small";
+  } else if (num < 15) {
+    return "Medium";
+  } else if (num < 20) {
+    return "Large";
+  } else {
+    return "Huge";
+  }
+}
+
+testSize(7);
+
+// 73
+var names = [
+  "Hole-in-one!",
+  "Eagle",
+  "Birdie",
+  "Par",
+  "Bogey",
+  "Double Bogey",
+  "Go Home!",
+];
+function golfScore(par, strokes) {
+  if (strokes === 1) {
+    return "Hole-in-one!";
+  } else if (strokes <= par - 2) {
+    return "Eagle";
+  } else if (strokes === par - 1) {
+    return "Birdie";
+  } else if (strokes === par) {
+    return "Par";
+  } else if (strokes === par + 1) {
+    return "Bogey";
+  } else if (strokes === par + 2) {
+    return "Double Bogey";
+  } else {
+    return "Go Home!";
+  }
+}
+
+golfScore(5, 4);
+
+// 74
+function caseInSwitch(val) {
+  var answer = "";
+
+  switch (val) {
+    case 1:
+      answer = "alpha";
+      break;
+
+    case 2:
+      answer = "beta";
+      break;
+
+    case 3:
+      answer = "gamma";
+      break;
+
+    case 4:
+      answer = "delta";
+      break;
+  }
+
+  return answer;
+}
+
+caseInSwitch(1);
+
+// 75
+function switchOfStuff(val) {
+  var answer = "";
+
+  switch (val) {
+    case "a":
+      answer = "apple";
+      break;
+
+    case "b":
+      answer = "bird";
+      break;
+
+    case "c":
+      answer = "cat";
+      break;
+
+    default:
+      answer = "stuff";
+  }
+
+  return answer;
+}
+
+switchOfStuff(1);
+
+// 76
+function sequentialSizes(val) {
+  var answer = "";
+  // Only change code below this line
+  switch (val) {
+    case 1:
+    case 2:
+    case 3:
+      answer = "Low";
+      break;
+
+    case 4:
+    case 5:
+    case 6:
+      answer = "Mid";
+      break;
+
+    case 7:
+    case 8:
+    case 9:
+      answer = "High";
+      break;
+  }
+  // Only change code above this line
+  return answer;
+}
+
+sequentialSizes(1);
+
+// 77
+function chainToSwitch(val) {
+  var answer = "";
+  // Only change code below this line
+  switch (val) {
+    case "bob":
+      answer = "Marley";
+      break;
+
+    case 42:
+      answer = "The Answer";
+      break;
+
+    case 1:
+      answer = "There is no #1";
+      break;
+
+    case 99:
+      answer = "Missed me by this much!";
+      break;
+
+    case 7:
+      answer = "Ate Nine";
+      break;
+  }
+  // Only change code above this line
+  return answer;
+}
+
+chainToSwitch(7);
+
+// 78
+function isLess(a, b) {
+  // Only change code below this line
+  return a < b;
+  // Only change code above this line
+}
+
+isLess(10, 15);
+
+// 79
+function abTest(a, b) {
+  // Only change code below this line
+  if (a < 0 || b < 0) {
+    return;
+  }
+  // Only change code above this line
+
+  return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+}
+
+abTest(2, 2);
+
+// 80
+var count = 0;
+
+function cc(card) {
+  // Only change code below this line
+  switch (card) {
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+      count += 1;
+      break;
+
+    case 10:
+    case "J":
+    case "Q":
+    case "K":
+    case "A":
+      count -= 1;
+      break;
+  }
+
+  if (count > 0) {
+    return `${count} Bet`;
+  }
+
+  return `${count} Hold`;
+  // Only change code above this line
+}
+
+cc(2);
+cc(3);
+cc(7);
+cc("K");
+cc("A");
