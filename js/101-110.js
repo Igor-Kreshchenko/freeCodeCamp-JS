11bad3f6d3b1b87ab86eadec7f0702e115935c07
Igor Kreshchenko <Igor.kreshchenko@gmail.com>
2021-01-17 12:08:02 +0200

    tasks 101-111

--- js/101-110.js
+++ js/101-110.js
@@ -1,1 +1,122 @@
 // 101
+function sum(arr, n) {
+  if (n <= 0) {
+    return 0;
+  } else {
+    return sum(arr, n - 1) + arr[n - 1];
+  }
+}
+
+// 102
+var contacts = [
+  {
+    firstName: "Akira",
+    lastName: "Laine",
+    number: "0543236543",
+    likes: ["Pizza", "Coding", "Brownie Points"],
+  },
+  {
+    firstName: "Harry",
+    lastName: "Potter",
+    number: "0994372684",
+    likes: ["Hogwarts", "Magic", "Hagrid"],
+  },
+  {
+    firstName: "Sherlock",
+    lastName: "Holmes",
+    number: "0487345643",
+    likes: ["Intriguing Cases", "Violin"],
+  },
+  {
+    firstName: "Kristian",
+    lastName: "Vos",
+    number: "unknown",
+    likes: ["JavaScript", "Gaming", "Foxes"],
+  },
+];
+
+function lookUpProfile(name, prop) {
+  for (const contact of contacts) {
+    if (contact.firstName === name) {
+      if (contact.hasOwnProperty(prop)) {
+        return contact[prop];
+      }
+
+      return "No such property";
+    }
+  }
+
+  return "No such contact";
+}
+
+console.log(lookUpProfile("Akira", "likes"));
+console.log(lookUpProfile("Kristian", "lastName"));
+console.log(lookUpProfile("Sherlock", "likes"));
+
+// 103
+function randomFraction() {
+  return Math.random();
+}
+
+// 104
+function randomWholeNum() {
+  return Math.floor(Math.random() * 10);
+}
+
+// 105
+function randomRange(myMin, myMax) {
+  return Math.floor(Math.random() * (myMax - myMin + 1) + myMin);
+}
+
+console.log(randomRange(1, 10));
+
+// 106
+function convertToInteger(str) {
+  return Number.parseInt(str);
+}
+
+convertToInteger("56");
+console.log(convertToInteger("007"));
+
+// 107
+function convertToInteger(str) {
+  return Number.parseInt(str, 2);
+}
+
+convertToInteger("10011");
+
+// 108
+function checkEqual(a, b) {
+  return a === b ? "Equal" : "Not Equal";
+}
+
+checkEqual(1, 2);
+
+// 109
+function checkSign(num) {
+  return num > 0 ? "positive" : num < 0 ? "negative" : "zero";
+}
+
+checkSign(10);
+
+// 110
+function countdown(n) {
+  if (n < 1) {
+    return [];
+  } else {
+    const countArray = countdown(n - 1);
+    countArray.unshift(n);
+    return countArray;
+  }
+}
+
+// 111
+function rangeOfNumbers(startNum, endNum) {
+  if (endNum - startNum === 0) {
+    return [startNum];
+  } else {
+    var numbers = rangeOfNumbers(startNum, endNum - 1);
+    numbers.push(endNum);
+    return numbers;
+  }
+}
